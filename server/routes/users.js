d1dcb3ebc8cc7be8e5814ed295afeff1c7a0692f
Amay <brajbliss@gmail.com>
2022-08-14 00:18:41 +0530

    mongodb cookies jwt authentication

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,35 @@
 import express from 'express';
-import {} from '../controllers/user.js';
+import {
+	deleteUser,
+	dislike,
+	getUser,
+	like,
+	subscribe,
+	unsubscribe,
+	update,
+} from '../controllers/user.js';
 
 const router = express.Router();
+
+// UPDATE USER
+router.put('/:id', update);
+
+// DELETE USER
+router.delete('/:id', deleteUser);
+
+// GET USER
+router.get('/find/:id', getUser);
+
+// SUBSCRIBE A USER
+router.put('/sub/:id', subscribe);
+
+// UNSUBSCRIBE A USER
+router.put('/unsub/:id', unsubscribe);
+
+// LIKE A VIDEO
+router.put('/like/:videoId', like);
+
+// DISLIKE A VIDEO
+router.put('/dislike/:videoId', dislike);
 
 export default router;
